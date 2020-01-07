b31ab12fe3bfc2c434268dab2d829644303a9423
Batuhan Saka <strawb3rryx7@gmail.com>
2020-01-08 02:20:12 +0300

    Initial Commit

---
+++ iTunesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTunesSwiftUI
+//
+//  Created by Batuhan Saka on 8.01.2020.
+//  Copyright © 2020 Batuhan Saka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
